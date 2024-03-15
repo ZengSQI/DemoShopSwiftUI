17a2cbb37bfa9a5d9efefde2d090aa5143e7334f
Steven Zeng <zengsqi199@gmail.com>
2024-03-16 05:10:48 +0800

    init

---
+++ DemoShopApp/DemoShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoShopAppApp.swift
+//  DemoShopApp
+//
+//  Created by Steven Zeng on 2024/3/16.
+//
+
+import SwiftUI
+
+@main
+struct DemoShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
